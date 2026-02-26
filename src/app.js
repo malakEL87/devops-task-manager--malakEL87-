ea481a9237da58bf725d59ce1b7d2f59be59b79a
malakEL87 <elarch.malak@etu.uae.ac.ma>
2026-02-26 21:49:04 +0000

    Test CI with small change

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
+
+// CI test change
+console.log("Testing CI with a Pull Request");
 const express = require('express');
 const app = express();
+
 app.use(express.json());
+
 const tasksRouter = require('./routes/tasks');
+
 app.use('/tasks', tasksRouter);
 
 app.get('/', (req, res) => {
-res.json({ message: "Welcome from MAIN branch" });
+  res.json({ message: "Welcome from MAIN branch" });
+});
 
-
-app.listen(3000, ()=> console.log("API running on port 3000"));
+app.listen(3000, () => {
+  console.log("API running on port 3000");
+});
